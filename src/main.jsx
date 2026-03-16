741ac91012f0c498c5eb69fbc3bb1b6667d9ef73
Eliseo <eliseo@vortexa.com>
2026-03-16 15:07:36 -0500

    fix: imports missing in main.jsx preventing app render

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { LanguageProvider } from './context/LanguageContext';
+import App from './App';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
